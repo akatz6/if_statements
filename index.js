0423fae5d00099af5cf4d0bbcf3f37337baf3217
akatz6 <aaronk488@gmail.com>
2023-04-10 17:05:56 -0700

    second example

--- index.js
+++ index.js
@@ -1,75 +1,76 @@
-// This is the function
+/*
+  The rules for fizzBuzzBooz
+  if the number is divisible by three with no remainder print out fizz
+  if the number is divisible by five with no remainder print out buzz
+  if the number is divisible by five and three with no remainder print out booz
+*/
+
+//This is a function, will explain more in another class
 const fizzBuzzBooz = () => {
   const hundred = 100;
-  // This is a loop
+  // This is a for loop, we will go into more detail Thursday night
   for (let i = 1; i <= hundred; i++) {
+    // This is a personal preference. This will make more sense when he get to arrays
     const index = i;
-    // Both statements are true
-    if (index % 3 === 0 && index % 5 === 0) {
+    if (index % 5 === 0 && index % 3 === 0) {
       console.log(
-        `${index} is divisible by three and five and does not have a remainder so booz `
+        `${index} is divisible by five and three with no remainder so booz`
       );
-    }
-    //  Either statements are true
-    else if (index % 3 === 0 || index % 5 === 0) {
+    } else if (index % 3 === 0 || index % 5 === 0) {
       if (index % 3 === 0) {
-        console.log(
-          `${index} is divisible by three and does not have a remainder so fizz `
-        );
-      } else if (index % 5 === 0) {
-        console.log(
-          `${index} is divisible by five and does not have a remainder so buzz`
-        );
+        console.log(`${index} is divisible by three with no remainder so fizz`);
+      } else {
+        console.log(`${index} is divisible by five with no remainder so buzz`);
       }
     }
   }
 };
 
-// fizzBuzzBooz is an algorithm question the is given
-// to junior developer during the interview process
-// If a number is divisible by three and has not remainder print "fizz"
-// If a number is divisible by five and has not remainder print "buzz"
-// If a number is divisible by both five and three and has not remainder print "booz"
-// This fizzBuzzBooz() is a function header, it calls a function
-console.log(fizzBuzzBooz());
+//This is a function header it call a function
+fizzBuzzBooz();
 
-const animal = "Cow";
+const animalSounds = (animal) => {
+  switch (animal) {
+    case "cow":
+      console.log("moo");
+      break;
+    case "sheep":
+      console.log("baa");
+      break;
+    default:
+      console.log("the animal sound has not been added yet");
+  }
+};
 
-switch (animal) {
-  case "Cow":
-    console.log("Moo");
-    break;
-  case "Sheep":
-    console.log("Baa");
-    break;
-  default:
-    console.log("Animal has not been added yet");
-}
+let animal = "cow";
+// This is a function header and this time we are passing in a parameter
+animalSounds(animal);
+animal = "sheep";
+animalSounds(animal);
+animal = "dog";
+animalSounds(animal);
 
 let bool = true;
 if (bool) {
   console.log("This is true");
 }
 bool = false;
-// putting a ! before a statement is looking for the opposite
+// ! in front of a boolean means you are looking for the opposite
 if (!bool) {
-  console.log("This is not true");
+  console.log("This is false");
 }
 
-// Never directly check is a value is zero
-// It thinks you are asking if it is false
-if (0) {
-  console.log("This is trap, do not do this");
+//Some traps to lookout for
+const number = 0;
+// This is looking for a false value not zero
+if (number) {
+  console.log("This is a trap don't do this");
 }
 
-// Same thing if you are checking for an empty string
-const str = "";
-if (str) {
-  console.log("Is this another trap, do not do this");
+if (number === 0) {
+  console.log("This is one way to check for zero");
 }
-// ternary statement
-// unless comparing to zero 
-// you can check without comparing to true or false
+
 const vanilla = true;
-const chocolate = vanilla ? false : true;
-console.log(chocolate)
+const chocolate  = vanilla ? false: true;
+console.log(`Is this chocolate ${chocolate}`)
